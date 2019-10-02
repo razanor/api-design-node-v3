50358e3ed382b4470f48e807c31a452957f534b3
Nazarii Repak <nazara.re22@gmail.com>
2019-10-02 19:07:49 +0300

    Generic controller added

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -30,11 +30,61 @@ export const getMany = model => async (req, res) => {
   }
 }
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  try {
+    const docs = await model.create({ ...req.body, createdBy: req.user._id })
 
-export const updateOne = model => async (req, res) => {}
+    res.status(201).json({ data: docs })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
-export const removeOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  try {
+    const updatedDoc = await model
+      .findOneAndUpdate(
+        {
+          createdBy: req.user._id,
+          _id: req.params.id
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec()
+
+    if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export const removeOne = model => async (req, res) => {
+  try {
+    const removedDoc = await model
+      .findOneAndRemove({
+        createdBy: req.user._id,
+        _id: req.params.id
+      })
+      .exec()
+
+    if (!removedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: removedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
