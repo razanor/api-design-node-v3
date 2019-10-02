a32f5dcbf0baa777ae478185aff9b3bb0adcd18d
Nazarii Repak <nazara.re22@gmail.com>
2019-10-02 16:48:09 +0300

    GetMany method added

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,6 +1,38 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  try {
+    const doc = await model
+      .findOne({ createdBy: req.user._id, _id: req.params.id })
+      .lean()
+      .exec()
 
-export const getMany = model => async (req, res) => {}
+    if (!doc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: doc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export const getMany = model => async (req, res) => {
+  try {
+    const doc = await model
+      .find({ createdBy: req.user._id })
+      .lean()
+      .exec()
+
+    if (!doc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: doc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
 export const createOne = model => async (req, res) => {}
 
