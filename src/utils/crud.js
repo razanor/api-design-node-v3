603071e4d2391b2d23e249d6208ffe2f71cb5daa
Nazarii Repak <nazara.re22@gmail.com>
2019-10-02 16:49:41 +0300

    GetMany method modified

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -18,16 +18,12 @@ export const getOne = model => async (req, res) => {
 
 export const getMany = model => async (req, res) => {
   try {
-    const doc = await model
+    const docs = await model
       .find({ createdBy: req.user._id })
       .lean()
       .exec()
 
-    if (!doc) {
-      return res.status(400).end()
-    }
-
-    res.status(200).json({ data: doc })
+    res.status(200).json({ data: docs })
   } catch (e) {
     console.error(e)
     res.status(400).end()
